d3e4544737dbefbc3a187ea7f28f414f33309b43
Mohamed Ibrahim <mohamedibrahim.fed@gmail.com>
2022-04-29 22:57:55 +0200

    first commit

--- src/Pages/MovieDetailsPage/MovieInfo.jsx
+++ src/Pages/MovieDetailsPage/MovieInfo.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const MovieInfo = ({ movieData, mediaType }) => {
+  useEffect(() => {
+    if (movieData) {
+      console.log(movieData);
+    }
+  }, [movieData]);
+  useEffect(() => {
+    console.log(mediaType);
+  }, [mediaType]);
   return (
     <div style={{ marginLeft: "2rem" }}>
       <h4> {mediaType === "tv" ? "series" : "movie"} Information</h4>
@@ -26,6 +34,38 @@ const MovieInfo = ({ movieData, mediaType }) => {
       )}
 
       <p>Status : {movieData.status} </p>
+      {mediaType === "tv" && movieData ? (
+        <>
+          <p> avarge time of the episode : {movieData?.episode_run_time} </p>
+          <p>last episode date : {movieData?.last_episode_to_air?.air_date}</p>
+          <p>
+            last episode number :{" "}
+            {movieData?.last_episode_to_air?.episode_number}
+          </p>
+          <p>last episode name : {movieData?.last_episode_to_air?.name}</p>
+          {movieData.next_episode_to_air ? (
+            <>
+              <p>
+                next episode date:
+                {movieData.next_episode_to_air.air_date}
+              </p>
+              <p>
+                next episode number :
+                {movieData.next_episode_to_air.episode_number}
+              </p>
+              <p>
+                next episode name :
+                {movieData.next_episode_to_air.name &&
+                movieData.next_episode_to_air.name !== ""
+                  ? movieData.next_episode_to_air.name
+                  : "unset"}
+              </p>{" "}
+            </>
+          ) : null}
+          <p> number of seasons : {movieData?.number_of_seasons} </p>
+          <p>number of total episodes : {movieData?.number_of_episodes} </p>
+        </>
+      ) : null}
     </div>
   );
 };
